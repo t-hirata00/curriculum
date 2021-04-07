d82829fcd09cae67bd2c0d88699abdf4fdce1729
t-hirata00 <pyude00@gmail.com>
2021-04-07 15:12:20 +0900

    1-4
    提出課題質問

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -41,28 +41,27 @@ public class Task1_4 {
          
         // ② 「name」の値が「USER_RNAME」と等しく、「 pass 」の値が「USER_PASSWORD」と等しい場合。
         //      定数を使用して「 ログイン成功です 」と出力してください。
-        if (name.equals(USER_RNAME) && pass.equals(USER_PASSWORD)){
+        if(name.equals(USER_RNAME) && pass.equals(USER_PASSWORD)){
             System.out.println(CONST_MSG_SUCCESS);
 
          // ③ 「USER_RNAME」の値のみ等しい場合。
          //     定数を使用して「 パスワードに誤りがあります。 」 と出力してください。
 
-        }   else if (name.equals(USER_RNAME) && !(pass.equals(USER_PASSWORD))){
+        } else if(!(pass.equals(USER_PASSWORD))){
             System.out.println(CONST_MSG_ERROR_PASS);
 
          // ④ 「USER_PASSWORD」の値のみ等しい場合。
          //定数を使用して「 名前に誤りがあります。 」と出力してください。
 
-        }  else if (!(name.equals(USER_RNAME)) && pass.equals(USER_PASSWORD)){
-           System.out.println(CONST_MSG_ERROR_NAME);
+        } else if(!(name.equals(USER_RNAME))){
+            System.out.println(CONST_MSG_ERROR_NAME);
 
          // ⑤ 「USER_RNAME」も「USER_PASSWORD」の値も間違っていた場合。 
          //定数を使用して「 入力情報に誤りがあります。 」と出力してください。
 
-        }  else if (!(name.equals(USER_RNAME)) && !(pass.equals(USER_PASSWORD))){
-           System.out.println(CONST_MSG_ERROR_INPUT);
+        } else {
+            System.out.println(CONST_MSG_ERROR_INPUT);
         }
-
 
 
     }
