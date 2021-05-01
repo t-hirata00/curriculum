4640aca25ec78430d82e5e39a7988e7cbce8af45
t-hirata00 <pyude00@gmail.com>
2021-05-02 00:22:12 +0900

    2-1

--- JavaStudy/2-1/src/study/Main.java
+++ JavaStudy/2-1/src/study/Main.java
@@ -19,7 +19,7 @@ public class Main {
     	
 
 
-       /** //getName関数の呼び出しと出力
+       //getName関数の呼び出しと出力
         System.out.println(getName(firstName, lastName));
         System.out.println();
 
@@ -27,28 +27,34 @@ public class Main {
 
         //③ arr 配列をループさせ、isOdd関数を使って配列の要素が奇数かどうかの判定を行いなさい。
         //   要素が奇数の場合には「nは奇数です。」と出力されます。
+        for(int i=0; i<arr.length ;i++) {
+            isOdd(arr[i]);
+        }
+
+        
 
 
 
-    }**/
+    }
 
     //② 関数を定義しなさい。
     //   関数名：getName
     //  修飾子：public
     //   引数には①で定義したfirstNameとlastNameを引数で受け取り、
     //  last_name と first_name を連結した値を返り値とする変数を定義しなさい。
-    public static getName (String firstName,String lastName) {
+    public static String getName(String firstName , String lastName){
+    	
     	return firstName + lastName;
     }
 
 
 
 
-    /** //奇数の判定を行う。
+    //奇数の判定を行う。
     public static void isOdd(int num) {
       if(num % 2 != 0) {
         System.out.println(num + "は奇数です。");
-        } **/
+        }
   }
-
 }
+
