e4b3f039deca99e2d2885e3ccb42d2a9c85c5b4b
t-hirata00 <pyude00@gmail.com>
2021-05-18 16:45:23 +0900

    2-5

---
+++ JavaStudy/2-5/src/study/Study.java
@@ -0,0 +1,60 @@
+package study;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 本課題では、コレクションフレームワークのList系クラスとMap系クラスの基本的な記述を学びましょう。
+ * 現場で必ず使用するものなので、ポイントをしっかり押さえておきましょう。
+ *
+ * 問①〜問③まであります。
+ * 問②に関しては、コメントを記述してください。
+ *
+ */
+public class Study {
+
+    public static final String SHOP_SHOHIN_00 = "バナナ";
+    public static final String SHOP_SHOHIN_01 = "牛乳";
+    public static final String SHOP_SHOHIN_02 = "豚肉";
+    public static final String SHOP_SHOHIN_03 = "コロッケ";
+
+    public static void main(String args[]) {
+
+        // ① 定数を全て使って、String型のListを記述してください。
+    	ArrayList <String>shohinList=new ArrayList<String>();
+    	shohinList.get(0);
+    	shohinList.get(1);
+    	shohinList.get(2);
+    	shohinList.get(3);
+
+
+
+
+
+
+        // ② 以下の「shopMap.put(shohinList.get(1), 180);」の処理について、コメントを記述してください。
+        /*
+         * [shohinList.get(1), 180)はペアで値を格納されている。キーがshohinList.get(1)で、値は180がかえってくる]
+         *
+         */
+    	LinkedHashMap<String, Integer> shopMap = new LinkedHashMap<String, Integer>();
+        shopMap.put(shohinList.get(0), 125);
+        shopMap.put(shohinList.get(1), 180);
+        shopMap.put(shohinList.get(2), 350);
+        shopMap.put(shohinList.get(3), 100);
+
+        // ③ カリキュラムを参考に拡張for文を使って、課題の画像と同じ表示になるよう記述してください。
+        // 「shohinList」と「shopMap」が保持する値を上手く利用しましょう。
+        
+        // entrySetを使用してMapに含まれる要素の「キー／値」のセットを取得
+        for (Map.Entry<String,Integer > entry : shopMap.entrySet()) {
+            System.out.println("キー: " + entry.getKey() + "／値: " + entry.getValue());
+        }
+
+
+
+
+
+
+    }
+}
