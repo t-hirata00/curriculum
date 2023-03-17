22478854101dae9bfa92ca669e23ebfa415afa01
t-hirata00 <pyude00@gmail.com>
2023-03-17 23:52:27 +0900

    8章

---
+++ PHPjob/8/Javascript/2-2/main.js
@@ -0,0 +1,5 @@
+// main.js
+let elems = document.querySelectorAll('input[type=radio]');
+for (let i = 0; i < elems.length; i++) {
+    console.log(elems[i]);
+}
---
+++ PHPjob/8/Javascript/2-3/main.js
@@ -0,0 +1,12 @@
+// main.js
+//box要素とbtn要素を取得
+let box = document.getElementById('box');
+let btn = document.getElementById('btn');
+//ボタンが押されたら、背景色を赤にする
+btn.addEventListener('click', function() {
+    box.style.backgroundColor = 'red';
+}, false);
+//ボタンが押されたら、アラートを出す
+btn.addEventListener('click', function() {
+    alert('ボタンが押されました');
+}, false);
